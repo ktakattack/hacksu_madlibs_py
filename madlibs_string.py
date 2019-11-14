585257bd8e8c610ab34e92931eef34989ee306dc
ktakattack <30424160+ktakattack@users.noreply.github.com>
2019-11-14 13:06:50 -0500

    Create madlibs_string.py

---
+++ madlibs_string.py
@@ -0,0 +1,11 @@
+#uses python 3
+noun1=input("enter a noun ")
+noun2=input("enter a noun ")
+verb1=input("enter a verb ")
+adj1=input("enter an adj ")
+verb2=input("enter a verb ")
+adj2=input("enter an adj ")
+
+STORY1= "%s Encourages %s To %s %s Packages By %s Cinder Block To %s Truck’s Accelerator"
+
+print(STORY1 % (noun1, noun2, verb1, adj1, verb2, adj2))
