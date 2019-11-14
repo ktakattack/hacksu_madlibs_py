de598fe54048872f450987662dc37bc4d2210762
ktakattack <30424160+ktakattack@users.noreply.github.com>
2019-11-14 13:14:24 -0500

    Create madlibs_func.py

---
+++ madlibs_func.py
@@ -0,0 +1,6 @@
+def user_input(noun1, noun2, verb1, adj1, verb2, adj2):
+  STORY1= "%s Encourages %s To %s %s Packages By %s Cinder Block To %s Truck’s Accelerator"
+  print (STORY1 % (noun1, noun2, verb1, adj1, verb2, adj2))
+
+
+user_input("Amazon", "drivers", "deliver", "great", "tying", "big")
